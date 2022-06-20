05299d392fa2ef67ae8b52e84a56be1ca5ecdf5a
Peter Wu <82991172+s3381402@users.noreply.github.com>
2022-06-20 15:32:32 +1000

    added page1 example template

--- Story.py
+++ Story.py
@@ -7,5 +7,9 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/page1")
+def page1():
+    return render_template('page1.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
